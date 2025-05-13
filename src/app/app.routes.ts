f3a39c0d276878412956c767ba602d0eb96783b4
Leyzanelle Pedregosa <S00222062@atu.ie>
2025-05-13 23:32:06 +0100

    created routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { GameSearchComponent } from './components/game-search/game-search.component';
+import { GameGalleryComponent } from './components/game-gallery/game-gallery.component';
+import { GameWishlistComponent } from './components/game-wishlist/game-wishlist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'',component:GameSearchComponent},
+    {path:'gallery',component:GameGalleryComponent},
+    {path:'wishlist',component:GameWishlistComponent},
+];
